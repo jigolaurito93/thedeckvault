5bb8065f1828981ce4d0f34731bf1fb6bd99d0cd
Jose Laurito <jigolaurito93@yahoo.com>
2024-09-02 19:05:25 -0500

    finishing up slider component

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const slides = [
   {
@@ -34,9 +34,20 @@ const slides = [
 const Slider = () => {
   const [current, setCurrent] = useState(0);
 
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setCurrent((prev) => (prev === slides.length - 1 ? 0 : prev + 1));
+  //   }, 3000);
+
+  //   return () => clearInterval(interval);
+  // }, []);
+
   return (
     <div className="h-[calc(100vh-80px)] overflow-hidden">
-      <div className="w-max h-full flex transition-all ease-in-out duration-1000">
+      <div
+        className="w-max h-full flex transition-all ease-in-out duration-1000"
+        style={{ transform: `translateX(-${current * 100}vw)` }}
+      >
         {slides.map((slide, index) => (
           <div
             key={slide.id}
@@ -68,6 +79,21 @@ const Slider = () => {
               />
             </div>
             {/* image container */}
+          </div>
+        ))}
+      </div>
+      <div className="absolute flex left-1/2 bottom-8 gap-4">
+        {slides.map((slide, index) => (
+          <div
+            className={`w-3 h-3 rounded-full ring-1 ring-gray-600 cursor-pointer flex items-center justify-center ${
+              current === index ? "scale-150" : ""
+            }`}
+            key={slide.id}
+            onClick={() => setCurrent(index)}
+          >
+            {current === index && (
+              <div className="w-[6px] h-[6px] bg-gray-600 rounded-full"></div>
+            )}
           </div>
         ))}
       </div>
