b1d266fc7e5c48a90449e3840127f5ffd276f631
Jose Laurito <jigolaurito93@yahoo.com>
2024-09-09 05:32:44 -0500

    changed with per breakpoint

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 const ProductList = () => {
   return (
-    <div className="mt-12 h-full grid grid-cols-12 w-full justify-evenly gap-y-10 2xl:px-32 xxl:px-80"  >
+    <div className="mt-12 h-full grid grid-cols-12 w-full justify-evenly gap-y-10 lg:w-[1082px] xl:w-[1300px] 2xl:w-[1600px]">
       <ProductListItem />
       <ProductListItem />
       <ProductListItem />
