bceb5f9bddff7cb256446cbccd78c48efe9a964e
Jose Laurito <jigolaurito93@yahoo.com>
2024-09-12 04:04:23 -0500

    testing supabase fetching

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,9 +1,16 @@
 import Campaign from "@/components/ListPage/Campaign";
 import FilterSection from "@/components/ListPage/FilterSection";
 import ProductList from "@/components/ProductList";
+import { createClient } from "@/utils/supabase/server";
+import { log } from "console";
 import React from "react";
 
-const ListPage = () => {
+const ListPage = async () => {
+  const supabase = createClient();
+
+  const { data, error } = await supabase.from("cards").select("*");
+  log(data, error);
+
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative border-4">
       <Campaign />
