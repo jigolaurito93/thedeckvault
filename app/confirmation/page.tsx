0a5b4ce35bb0c46ef89b491c6d06ef8277bdada3
Jose Laurito <jigolaurito93@yahoo.com>
2024-09-23 22:37:52 -0500

    confirmation page mobile

---
+++ app/confirmation/page.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import { FaCheckCircle } from "react-icons/fa";
+
+const ConfirmationPage = () => {
+  return (
+    <div className="flex flex-col justify-center items-center px-10 text-center gap-6 py-16">
+      <FaCheckCircle className="text-green-500 text-4xl" />
+      <h1 className="text-xl font-semibold">Thank you for registering!</h1>
+      <h2 className=" text-sm">
+        Please check your email to verify your account. Follow the link in the
+        email to complete your registration and access your account.
+      </h2>
+      <div className="text-xs">
+        If you don't see the email, check your spam or junk folder.
+      </div>
+    </div>
+  );
+};
+
+export default ConfirmationPage;
