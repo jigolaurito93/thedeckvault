2a810ed2e91700e41e1f8f29e64cff1fd92ead6d
Jose Laurito <jigolaurito93@yahoo.com>
2024-09-23 23:58:11 -0500

    login page revamp

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,15 +1,61 @@
-import { login, signup } from "./actions";
+"use client";
+import { createClient } from "@/utils/supabase/client";
+import { login } from "./actions";
+import { FcGoogle } from "react-icons/fc";
 
 const LoginPage = () => {
+  const handleGoogleRegister = async () => {
+    const supabase = createClient();
+    await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: "http://localhost:3000/auth/callback",
+      },
+    });
+  };
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
+    <div>
+      <div className="py-20 w-[80vw] max-w-[25rem] mx-auto flex flex-col items-center gap-5 ">
+        <h1 className=" text-2xl text-center">
+          Welcome back! Log in to your account
+        </h1>
+        <form action="" className="flex flex-col w-full gap-2">
+          <label htmlFor="email" className="pl-2">
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Enter your email"
+            className="border rounded-sm p-2"
+          />
+          <label htmlFor="password" className="pl-2">
+            Password
+          </label>
+          <input
+            type="text"
+            name="password"
+            id="password"
+            placeholder="•••••••••"
+            className="border rounded-sm p-2"
+          />
+          <button type="submit" className="p-2 border" formAction={login}>
+            Log In
+          </button>
+        </form>
+        or
+        {/* OAuth */}
+        <button
+          className="flex items-center gap-2 border w-full justify-center py-2"
+          onClick={() => handleGoogleRegister()}
+        >
+          <FcGoogle className="text-xl" />
+          <h3>Sign Up with Google</h3>
+        </button>
+        {/* OAuth */}
+      </div>
+    </div>
   );
 };
 
