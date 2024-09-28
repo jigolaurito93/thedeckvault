3fb4a661f6e7253cdee6137f259d28133fe42094
Jose Laurito <jigolaurito93@yahoo.com>
2024-09-28 04:42:54 -0500

    created a table that displays info for products

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { createClient } from "@/utils/supabase/client";
 import React, { useEffect, useState } from "react";
+import { PokemonCards } from "@/data/pokemon-cards";
+import Image from "next/image";
 
 interface Product {
   id: number;
@@ -10,6 +12,10 @@ interface Product {
   category_id: number;
   created_at: string;
   updated_at: string;
+}
+
+interface PokemonCard {
+  [key: string]: { imageUrl: string; description: string };
 }
 
 const productTitle = [
@@ -22,6 +28,13 @@ const productTitle = [
   "Created At",
   "Updated At",
 ];
+
+const getPokemonCardData = (name: string) => {
+  const card = PokemonCards.find((card) => card.name === name);
+  return card
+    ? { imageUrl: card.image, description: card.description }
+    : undefined;
+};
 
 const AdminProducts = () => {
   const [products, setProducts] = useState<Product[]>([]);
@@ -69,13 +82,27 @@ const AdminProducts = () => {
               created_at,
               updated_at,
             } = product;
+            const pokemonCardData = getPokemonCardData(name);
+
             return (
               <tr key={product.id}>
                 <td className="border border-black px-4 py-2">{id}</td>
-                <td className="border border-black px-4 py-2"></td>
+                <td className="border border-black px-4 py-2">
+                  {pokemonCardData?.imageUrl && (
+                    <Image
+                      src={pokemonCardData.imageUrl}
+                      alt={name}
+                      className="w-20 h-28 mx-auto"
+                      width={300}
+                      height={300}
+                    />
+                  )}
+                </td>
                 <td className="border border-black px-4 py-2">{name}</td>
                 <td className="border border-black px-4 py-2">${price}</td>
-                <td className="border border-black px-4 py-2">{description}</td>
+                <td className="border border-black px-4 py-2">
+                  {pokemonCardData?.description || description}
+                </td>
                 <td className="border border-black px-4 py-2">{category_id}</td>
                 <td className="border border-black px-4 py-2">
                   {new Date(created_at).toLocaleDateString()}
---
+++ data/pokemon-cards.ts
@@ -0,0 +1,183 @@
+export const PokemonCards = [
+    {
+      name: "Alakazam",
+      image: "https://images.pokemontcg.io/base1/1_hires.png",
+      description:
+        "Its brain can outperform a supercomputer. Its intelligence quotient is said to be 5000.",
+    },
+    {
+      name: "Blastoise",
+      image: "https://images.pokemontcg.io/base1/2_hires.png",
+      description:
+        "A brutal Pokémon with pressurized water jets on its shell. They are used for high-speed tackles.",
+    },
+    {
+      name: "Chansey",
+      image: "https://images.pokemontcg.io/base1/3_hires.png",
+      description:
+        "A rare and elusive Pokémon that is said to bring happiness to those who manage to catch it.",
+    },
+    {
+      name: "Charizard",
+      image: "https://images.pokemontcg.io/base1/4_hires.png",
+      description:
+        "Spits fire that is hot enough to melt boulders. Known to unintentionally cause forest fires.",
+    },
+    {
+      name: "Clefairy",
+      image: "https://images.pokemontcg.io/base1/5_hires.png",
+      description:
+        "Its magical and cute appeal has many admirers. It is rare and found only in certain areas.",
+    },
+    {
+      name: "Gyarados",
+      image: "https://images.pokemontcg.io/base1/6_hires.png",
+      description:
+        "Rarely seen in the wild. Huge and vicious, it is capable of destroying entire cities in a rage.",
+    },
+    {
+      name: "Hitmonchan",
+      image: "https://images.pokemontcg.io/base1/7_hires.png",
+      description:
+        "While seeming to do nothing, it fires punches in lightning-fast volleys that are impossible to see.",
+    },
+    {
+      name: "Machamp",
+      image: "https://images.pokemontcg.io/base1/8_hires.png",
+      description:
+        "Using its amazing muscles, it throws powerful punches that can knock its victim clear over the horizon.",
+    },
+    {
+      name: "Magneton",
+      image: "https://images.pokemontcg.io/base1/9_hires.png",
+      description:
+        "Formed by several Magnemites linked together. It frequently appears when sunspots flare up.",
+    },
+    {
+      name: "Mewtwo",
+      image: "https://images.pokemontcg.io/base1/10_hires.png",
+      description:
+        "A scientist created this Pokémon after years of horrific gene-splicing and DNA engineering experiments.",
+    },
+    {
+      name: "Nidoking",
+      image: "https://images.pokemontcg.io/base1/11_hires.png",
+      description:
+        "Uses its powerful tail in battle to smash, constrict, then break its prey's bones.",
+    },
+    {
+      name: "Ninetales",
+      image: "https://images.pokemontcg.io/base1/12_hires.png",
+      description:
+        "Very smart and very vengeful. Grabbing one of its many tails could result in a 1,000-year curse.",
+    },
+    {
+      name: "Poliwrath",
+      image: "https://images.pokemontcg.io/base1/13_hires.png",
+      description:
+        "An adept swimmer at both the front crawl and breaststroke. Easily overtakes the best human swimmers.",
+    },
+    {
+      name: "Raichu",
+      image: "https://images.pokemontcg.io/base1/14_hires.png",
+      description:
+        "Its long tail serves as a ground to protect itself from its own high-voltage power.",
+    },
+    {
+      name: "Venusaur",
+      image: "https://images.pokemontcg.io/base1/15_hires.png",
+      description:
+        "This plant blooms when it is absorbing solar energy. It stays on the move to seek sunlight.",
+    },
+    {
+      name: "Zapdos",
+      image: "https://images.pokemontcg.io/base1/16_hires.png",
+      description:
+        "A legendary bird Pokémon said to appear from clouds while wielding enormous lightning bolts.",
+    },
+    {
+      name: "Beedrill",
+      image: "https://images.pokemontcg.io/base1/17_hires.png",
+      description:
+        "Flies at high speed and attacks using the large, venomous stingers on its forelegs and tail.",
+    },
+    {
+      name: "Dragonair",
+      image: "https://images.pokemontcg.io/base1/18_hires.png",
+      description:
+        "A mystical Pokémon that exudes a gentle aura. Has the ability to change climate conditions.",
+    },
+    {
+      name: "Dugtrio",
+      image: "https://images.pokemontcg.io/base1/19_hires.png",
+      description:
+        "A team of Diglett triplets. It triggers huge earthquakes by burrowing 60 miles underground.",
+    },
+    {
+      name: "Electabuzz",
+      image: "https://images.pokemontcg.io/base1/20_hires.png",
+      description:
+        "Normally found near power plants, it can wander away and cause major blackouts in cities.",
+    },
+    {
+      name: "Electrode",
+      image: "https://images.pokemontcg.io/base1/21_hires.png",
+      description:
+        "It stores electrical energy under very high pressure. It often explodes with little or no provocation.",
+    },
+    {
+      name: "Pidgeotto",
+      image: "https://images.pokemontcg.io/base1/22_hires.png",
+      description:
+        "Very protective of its sprawling territory, this Pokémon will fiercely peck at any intruder.",
+    },
+    {
+      name: "Arcanine",
+      image: "https://images.pokemontcg.io/base1/23_hires.png",
+      description:
+        "A Pokémon that has been long admired for its beauty. It runs gracefully, as if on wings.",
+    },
+    {
+      name: "Charmeleon",
+      image: "https://images.pokemontcg.io/base1/24_hires.png",
+      description:
+        "When it swings its burning tail, it raises the temperature to unbearably high levels.",
+    },
+    {
+      name: "Dewgong",
+      image: "https://images.pokemontcg.io/base1/25_hires.png",
+      description:
+        "Stores thermal energy in its body. Swims at a steady 8 knots even in intensely cold waters.",
+    },
+    {
+      name: "Dratini",
+      image: "https://images.pokemontcg.io/base1/26_hires.png",
+      description:
+        "Long considered a mythical Pokémon until recently, when a small colony was found living underwater.",
+    },
+    {
+      name: "Farfetch'd",
+      image: "https://images.pokemontcg.io/base1/27_hires.png",
+      description:
+        "The sprig of green onions it holds is its weapon. This sprig is used much like a metal sword.",
+    },
+    {
+      name: "Growlithe",
+      image: "https://images.pokemontcg.io/base1/28_hires.png",
+      description:
+        "Very protective of its territory. It will bark and bite to repel intruders from its space.",
+    },
+    {
+      name: "Haunter",
+      image: "https://images.pokemontcg.io/base1/29_hires.png",
+      description:
+        "Because of its ability to slip through block walls, it is said to be from another dimension.",
+    },
+    {
+      name: "Ivysaur",
+      image: "https://images.pokemontcg.io/base1/30_hires.png",
+      description:
+        "When the bulb on its back grows large, it appears to lose the ability to stand on its hind legs.",
+    },
+  ];
+  
