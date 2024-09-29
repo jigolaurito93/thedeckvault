cc33574217b015f986793aa392b726c527c77a71
jigolaurito93 <jigolaurito93@yahoo.com>
2024-09-29 11:34:01 -0500

    create a user page

---
+++ app/profile/create-user/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CreateProfile = () => {
+  return <div>
+    <div>
+      <label>First Name</label>
+      <input type="text" />
+    </div>
+  </div>;
+};
+
+export default CreateProfile;
