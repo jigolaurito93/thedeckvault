6131b4a3ce9d20ba319ea953c8762ad1a35a3f5b
jigolaurito93 <jigolaurito93@yahoo.com>
2024-10-03 20:20:49 -0500

    added props for the user data

---
+++ utils/userDataProps.ts
@@ -0,0 +1,16 @@
+export interface UserDataProps {
+  id: string;
+  first_name: string;
+  last_name: string;
+  displayName: string;
+  email: string;
+  image_url: string;
+  role: string;
+  address1: string;
+  address2: string;
+  city: string;
+  state: string;
+  zipcode: string;
+  updatedAt: string;
+  createdAt: string;
+}
