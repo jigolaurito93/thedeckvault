ae3b133d3f3064993e1e8dd3187fc0789ccf794d
jigolaurito93 <jigolaurito93@yahoo.com>
2024-10-11 00:09:04 -0500

    Links for navbar mobile

---
+++ data/navbar/mobile-links.ts
@@ -0,0 +1,31 @@
+export const mobileLinks = [
+  {
+    title: "Home",
+    url: "/",
+  },
+  {
+    title: "Shop",
+    url: "/",
+  },
+  {
+    title: "Deals",
+    url: "/",
+  },
+
+  {
+    title: "About",
+    url: "/",
+  },
+  {
+    title: "Contact",
+    url: "/",
+  },
+  {
+    title: "Logout",
+    url: "/",
+  },
+  {
+    title: "Cart(1",
+    url: "/",
+  },
+];
